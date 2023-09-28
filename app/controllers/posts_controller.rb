5a97af7b8d9a2c56648e876fe34d73a16b24a298
Naokichi55 <ibosanganba@gmail.com>
2023-09-29 06:24:15 +0900

    新規投稿フォームを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,16 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.order(id: :asc).limit(10)
   end
 
   def create
+    post = Post.create!(post_params)
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+
 end

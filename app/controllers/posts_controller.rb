1e3594bcdef6c68cc8bdb89f49822473bbf1ef8b
Naokichi55 <ibosanganba@gmail.com>
2023-10-02 10:34:15 +0900

    ページネーションを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
  
+  PER_PAGE = 10
+
   def index
-    @posts = Post.order(id: :asc).limit(10)
+    @posts = Post.order(id: :desc).page(params[:page]).per(PER_PAGE)
   end
 
   def create
